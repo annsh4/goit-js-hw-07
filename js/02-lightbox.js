43cf5ca9aa355b2a0d76f62e4e49eaf7a1a63f43
Anna Tereshchuk <annaterr013@gmail.com>
2023-03-29 01:15:11 +0100

    render images

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallaryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+gallaryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+		<li>
+			<a class="gallery__link" href="${original}">
+				<img
+					class="gallery__image"
+					src="${preview}"
+					alt="${description}"
+				/>
+			</a>
+		</li>`;
+    })
+    .join("");
+}
